dbbfb6b1e05a7cc52ed8b796b392653a844537cf
kriuchkovaleks <103981431+kriuchkovaleks@users.noreply.github.com>
2022-06-05 10:15:56 +0300

    Add files via upload

---
+++ Task50.cs
@@ -0,0 +1,68 @@
+﻿/* Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет*/
+
+int GetDemension(string message)
+{
+    Console.WriteLine(message);
+    int demension = int.Parse(Console.ReadLine());
+    return demension;
+}
+
+int[,] InitMatrix(int firstDemension, int secondDemension)
+{
+    int[,] matrix = new int[firstDemension, secondDemension];
+    Random rnd = new Random();
+    for (int i = 0; i < firstDemension; i++)
+    {
+        for (int j = 0; j < secondDemension; j++)
+            matrix[i, j] = rnd.Next(0, 10);
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine("Заполненная матрица");
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} ");
+
+        Console.WriteLine();
+    }
+}
+
+
+void CheckNumber(int[,] matrix, int rowPosition, int columnPosition)
+{
+    int row = rowPosition;
+    int column = columnPosition;
+
+    if ((row > matrix.GetLength(0)) || (column > matrix.GetLength(1)))
+    {
+        Console.WriteLine("Такого числа в массиве нет");
+    }
+    else
+    {
+        Console.WriteLine(matrix[row, column]);
+    }
+}
+
+int firstDemension = GetDemension("Введите длину для первого разряда матрицы:");
+int secondDemension = GetDemension("Введите длину для второго разряда матрицы:");
+
+int[,] resultMatrix = InitMatrix(firstDemension, secondDemension);
+PrintMatrix(matrix: resultMatrix);
+
+Console.WriteLine("Введите номер строки");
+int row = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите номер столбца");
+int column = int.Parse(Console.ReadLine());
+
+CheckNumber(resultMatrix, row, column);
---
+++ Task52.cs
@@ -0,0 +1,68 @@
+﻿/* Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+
+int GetDemension(string message)
+{
+    Console.WriteLine(message);
+    int demension = int.Parse(Console.ReadLine());
+    return demension;
+}
+
+int[,] InitMatrix(int firstDemension, int secondDemension)
+{
+    int[,] matrix = new int[firstDemension, secondDemension];
+    Random rnd = new Random();
+    for (int i = 0; i < firstDemension; i++)
+    {
+        for (int j = 0; j < secondDemension; j++)
+            matrix[i, j] = rnd.Next(0, 10);
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine("Заполненная матрица");
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]       } ");
+
+        Console.WriteLine();
+    }
+}
+
+
+
+void GetAverageSumm(int[,] matrix)
+{
+    double summ = 0;
+    int j = 0;
+    while (j < matrix.GetLength(1))
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            summ = (summ + matrix[i, j]);
+        }
+        Console.WriteLine($" Среднеарифметическое в столбце {j} равно: {summ / matrix.GetLength(0)}");
+        j++;
+        summ = 0;
+    }
+}
+
+
+int firstDemension = GetDemension("Введите длину для первого разряда матрицы:");
+int secondDemension = GetDemension("Введите длину для второго разряда матрицы:");
+
+int[,] resultMatrix = InitMatrix(firstDemension, secondDemension);
+PrintMatrix(matrix: resultMatrix);
+
+GetAverageSumm(resultMatrix);
